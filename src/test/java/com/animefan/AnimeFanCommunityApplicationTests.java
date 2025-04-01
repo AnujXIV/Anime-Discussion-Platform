b9e34c8e3766975524362e64542f60cff77ff99b
Anuj <anujpatelx003@gmail.com>
2025-04-01 10:21:11 -0400

    first commit

---
+++ src/test/java/com/animefan/AnimeFanCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animefan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeFanCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
